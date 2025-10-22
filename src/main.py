1dda57e44cb7e7301f27737e87942c5b2ca6db22
piero <piero.anic@gmail.com>
2025-10-22 12:13:09 +0200

    + chapter one - intro to neural networks

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from network import Network
+import mnist_loader
+
+def _main():
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    N = Network([784, 30, 10])
+    N.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+if __name__ == '__main__':
+    _main()
